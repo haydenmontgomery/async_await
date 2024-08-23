331a574b4b2c82f292d2e69b8324c9ebebda2cf5
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-08-23 16:56:26 -0400

    Finished the card drawing async functions

--- deck_api.js
+++ deck_api.js
@@ -1,80 +1,49 @@
-function get(url) {
-    const request = new XMLHttpRequest();
-    return new Promise((resolve, reject) => {
-      request.onload = function () {
-        if (request.readyState !== 4) return;
+let baseURL = 'https://deckofcardsapi.com/api/deck';
+let div = document.getElementById('cards');
 
-        // Check status code
-        if (request.status >= 200 && request.status < 300) {
-          resolve({
-            data: JSON.parse(request.response),
-            status: request.status,
-            request: request,
-          })
-        } else {
-          reject({
-            msg: 'Server Error',
-            status: request.status,
-            request: request
-          })
-        }
-      }
-      request.onerror = function handleError() {
-        reject({
-          msg: 'NETWORK ERROR!'
-        })
-      };
-      request.open('GET', url);
-      request.send();
-    })
-}
-let div = document.getElementById('cards')
-
-
-get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-.then(res => {
-      console.log(res.data);
-      let deck_id = res.data.deck_id
-      return get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-    })
-    .then(res => {
-      console.log(res.data)
-      let deck_id = res.data.deck_id
-      console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`)
-      return get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-    })
-    .then(res => {
-      console.log(res.data)
-      console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`)
-    })
-    .catch(err => console.log(err))
-    
-let newDeck = true
-
-let deck = ''
-let newDeckId = ''
-document.getElementById('click').addEventListener('click', () => {
-  if (newDeck == true) {
-    newDeck = false;
-    get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res => {
-      console.log(res.data);
-      newDeckId = res.data.deck_id
-      return get(`https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=1`)
-    })
-    .then(res => {
-      console.log(res.data)
-      addCard(res.data.cards[0].image);
-    })
-  } else {
-    get(`https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=1`)
-    .then(res => {
-      console.log(res.data);
-      addCard(res.data.cards[0].image);
-    })
-
+async function shuffle_draw_one(){
+  let deck_id = ''
+  try {
+    let res = await axios.get(`${baseURL}/new/shuffle/?deck_count=1`)
+    deck_id = res.data.deck_id;
+  } catch(e) {
+    console.log(`Error in shuffle: ${e}`);
   }
-})
+  try {
+    let res = await axios.get(`${baseURL}/${deck_id}/draw/?count=1`);
+    console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`)
+  } catch(e){
+    console.log(`Error in draw: ${e}`);
+  }
+}
+
+shuffle_draw_one();
+
+async function shuffle_draw_two(){
+  let deck_id = ''
+  try {
+    let res = await axios.get(`${baseURL}/new/shuffle/?deck_count=1`)
+    deck_id = res.data.deck_id;
+  } catch(e) {
+    console.log(`Error in shuffle: ${e}`);
+  }
+  try {
+    let draws = await Promise.all([
+      $.getJSON(`${baseURL}/${deck_id}/draw/?count=1`),
+      $.getJSON(`${baseURL}/${deck_id}/draw/?count=1`)
+    ]);
+    for (card in draws) {
+      console.log(`${draws[card].cards[0].value} of ${draws[card].cards[0].suit}`)
+    }
+  } catch(e){
+    console.log(`Error in draw: ${e}`);
+  }
+}
+
+shuffle_draw_two()
+
+let newDeck = true;
+let click_deck_id = '';
 
 function addCard(url){
   img = document.createElement('img');
@@ -84,3 +53,32 @@ function addCard(url){
   img.style.transform = `rotate(${randomAngle}deg)`;
   div.appendChild(img);
 }
+
+async function card_clicker() {
+  if (newDeck == true){
+    newDeck = false;
+    try {
+      let res = await axios.get(`${baseURL}/new/shuffle/?deck_count=1`)
+      click_deck_id = res.data.deck_id;
+    } catch(e) {
+      console.log(`Error in shuffle: ${e}`);
+    }
+    try {
+      let res = await axios.get(`${baseURL}/${click_deck_id}/draw/?count=1`);
+      addCard(res.data.cards[0].image)
+    } catch(e){
+      console.log(`Error in draw: ${e}`);
+    }
+  } else {
+    try {
+      let res = await axios.get(`${baseURL}/${click_deck_id}/draw/?count=1`);
+      addCard(res.data.cards[0].image)
+    } catch(e){
+      console.log(`Error in draw: ${e}`);
+    }
+  }
+}
+
+document.getElementById('click').addEventListener('click', () => {
+  card_clicker();
+})
